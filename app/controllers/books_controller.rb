d2eacab5ed532dbbfa9e1c059102c6f1f091b924
fujii dan <2607dan@gmail.com>
2021-03-29 13:00:35 +0900

    before_action rescue_fromなどについて

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,35 @@
+class BooksController < ApplicationController
+  protect_from_forgery except: [:destroy]
+  before_acttion :set_book, only: [:show, :destroy]
+  around_action :action_logger, only: [:destroy]
+  before_action do
+    redirect_to access_denied_path if params[:token].blank?
+  end
+      
+  def show
+    respond_to do |format|
+      format.html
+      format.json
+    end
+  end
+
+  def destroy
+    @book.destroy
+    respond_to do |format|
+      format.html {redirect_to "/"}
+      format.json { head :no_content}
+    end
+  end  
+  
+  private
+
+  def set_book
+    @book = Book.find(params[:id])
+  end
+
+  def action_logger
+    logger.info "around-before"
+    yield
+    logger.info "around-after"
+  end  
+end
